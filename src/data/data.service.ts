2a51632457ecfb171509f271fc376bce2b6b0c57
Stepan Tulaidan <stepan.tulaidan@omnio.cz>
2024-08-25 10:17:43 +0200

    feat(seed): integrate faker to generate initial customer data

--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -1,14 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Customer } from './customer.entity';
+import { faker } from '@faker-js/faker';
 
 @Injectable()
-export class DataService {
+export class DataService implements OnModuleInit {
   constructor(
     @InjectRepository(Customer)
     private customerRepository: Repository<Customer>,
   ) {}
+
+  async onModuleInit() {
+    if ((await this.customerRepository.count()) === 0) {
+      const customers = Array(10)
+        .fill(null)
+        .map(() => {
+          const customer = new Customer();
+          customer.name = faker.person.fullName();
+          customer.email = faker.internet.email();
+          customer.phone = faker.phone.number();
+          return customer;
+        });
+      await this.customerRepository.save(customers);
+    }
+  }
 
   async findAll(): Promise<Customer[]> {
     return await this.customerRepository.find();
@@ -25,5 +41,9 @@ export class DataService {
   async update(id: number, customer: Partial<Customer>): Promise<Customer> {
     await this.customerRepository.update(id, customer);
     return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.customerRepository.delete(id);
   }
 }
